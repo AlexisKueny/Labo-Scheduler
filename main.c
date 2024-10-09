9c6bd9e9f70f14129d4d44d173d18360c8012308
alexi <alexis.kueny@outlook.com>
2024-10-09 10:16:42 +0200

    File output stream implemented

--- main.c
+++ main.c
@@ -13,12 +13,12 @@ typedef struct ProcessInfo {
     int priority;
 } processInfo_t;
 
-typedef struct ProcessOut {
+typedef struct ProcessExec {
     int pid;
     int turnAroundTime;
     int waitTime;
     int nTimes;
-} processOut_t;
+} processExec_t;
 
 typedef struct ProcessPerf {
     int total_time;
@@ -31,11 +31,11 @@ struct Data {
 };
 
 // Fist come first serve algorithm
-processOut_t fCFS (processInfo_t process, int completionTime) {
+processExec_t fCFS (processInfo_t process, int completionTime) {
     int turnAroundTime = completionTime - process.arrTime;
     int waitingTime = turnAroundTime - process.execTime;
-    processOut_t processOut = {process.pid,turnAroundTime,waitingTime,0};
-    return processOut;
+    processExec_t process_exec = {process.pid,turnAroundTime,waitingTime,0};
+    return process_exec;
 }
 
 void simulate(char filepath[], int algo) {
@@ -44,11 +44,14 @@ void simulate(char filepath[], int algo) {
     char *elem1, *elem2, *elem3, *elem4;
     int completionTime = 0;
     int turnAroundTime,waitingTime,executionTime,priority;
-    processOut_t process_out = {0,0,0,0};
+    processExec_t process_out = {0,0,0,0};
     processInfo_t process_info = {0,0,0,0};
 
-    // Reads file line by line
+    // File streams
     FILE *file = fopen(filepath, "r");
+    FILE *myStream = fopen("C:\\Users\\alexi\\CLionProjects\\Scheduler\\execution.csv","w");
+
+    // Reads file line by line
     while (fgets(line, sizeof(line), file)) {
         // Fill out the process structure
         elem1 = strtok(line, " ");
@@ -61,7 +64,7 @@ void simulate(char filepath[], int algo) {
 
         // Execute algorithm
         switch (algo) {
-            // First come first serve algorithm
+            // First come, first served algorithm
             case 1:
                 process_out = fCFS(process_info,completionTime);
                 break;
@@ -75,6 +78,10 @@ void simulate(char filepath[], int algo) {
                 break;
         }
         printf("Output Algo1: PID: %d, Turnaround time: %d, Waiting time: %d, Preempted times: %d \n",
+            process_out.pid,process_out.turnAroundTime,process_out.waitTime,process_out.nTimes);
+
+        // Write result into output file
+        fprintf(myStream, "%d %d %d %d\n",
             process_out.pid,process_out.turnAroundTime,process_out.waitTime,process_out.nTimes);
     }
     fclose(file);
